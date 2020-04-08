c5e16675c940c12dfed8b64945c7bfe7fe3778e8
Wetzel <manuel.wetzel@hpe.com>
2020-04-08 16:21:50 +0200

    fixed fraction rounding error and fixed wrong heurstic for wald

--- hiking/Heuristic.java
+++ hiking/Heuristic.java
@@ -18,14 +18,13 @@ public class Heuristic implements Scorer<Square> {
 		}
 
 		// find multiplicator, so that heuristic is optimistic
-		if (lowestTerrainCost + 3 / 2 < terrains.get(5).getCost()
-				&& lowestTerrainCost + 4 / 2 < terrains.get(1).getCost()) {
+		if (lowestTerrainCost + 1.5 < terrains.get(5).getCost() && lowestTerrainCost + 2 < terrains.get(1).getCost()) {
 			multiplicator = lowestTerrainCost;
 		} else {
-			if (terrains.get(5).getCost() - 3 / 2 > terrains.get(1).getCost() - 4 / 2) {
-				multiplicator = terrains.get(1).getCost() - 4 / 2 - 0.1;
+			if (terrains.get(5).getCost() - 1.5 > terrains.get(1).getCost() - 2) {
+				multiplicator = terrains.get(1).getCost() - 2 - 0.1;
 			} else {
-				multiplicator = terrains.get(5).getCost() - 3 / 2 - 0.1;
+				multiplicator = terrains.get(5).getCost() - 1.5 - 0.1;
 			}
 		}
 	}
@@ -34,13 +33,13 @@ public class Heuristic implements Scorer<Square> {
 	public HeuristicResult computeCost(Square from, Square to, double currentExhaustionPoints) {
 		if (from.getTerrain().getCode() == 3) { // Wald
 			result.setHeuristic(Math.hypot(from.getX() - to.getX(), from.getY() - to.getY()) * multiplicator
-					- currentExhaustionPoints / 2);
-			result.setExhaustionPoints(currentExhaustionPoints / 2);
+					- currentExhaustionPoints / 4.0);
+			result.setExhaustionPoints(currentExhaustionPoints / 2.0);
 		} else if (from.getTerrain().getCode() == 5) { // Felswand
-			result.setHeuristic(Math.hypot(from.getX() - to.getX(), from.getY() - to.getY()) * multiplicator + 3 / 2);
+			result.setHeuristic(Math.hypot(from.getX() - to.getX(), from.getY() - to.getY()) * multiplicator + 1.5);
 			result.setExhaustionPoints(currentExhaustionPoints + 3);
 		} else if (from.getTerrain().getCode() == 1) { // Fluss
-			result.setHeuristic(Math.hypot(from.getX() - to.getX(), from.getY() - to.getY()) * multiplicator + 4 / 2);
+			result.setHeuristic(Math.hypot(from.getX() - to.getX(), from.getY() - to.getY()) * multiplicator + 2);
 			result.setExhaustionPoints(currentExhaustionPoints + 4);
 		} else {
 			result.setHeuristic(Math.hypot(from.getX() - to.getX(), from.getY() - to.getY()) * multiplicator);
